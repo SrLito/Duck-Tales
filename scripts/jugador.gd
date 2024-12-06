11987783a5ef8a539491c010fa0fe6c16633f751
SrLito <74510505+SrLito@users.noreply.github.com>
2024-12-06 01:11:41 -0300

    Salto con baston.
    Realice la mecánica principal del juego (salto que rebota constantemente) pero toma únicamente el suelo (staticbody2d).

--- scripts/jugador.gd
+++ scripts/jugador.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 @export var velocidad = 100
 @export var salto = -100
 @export var gravedad = 980
+@export var saltoPogo = -500
+
+var saltoBaston = false
 
 func _physics_process(delta):
 	
@@ -20,7 +23,17 @@ func _physics_process(delta):
 		velocity.y = velocity.y + gravedad * delta
 	
 	# Aplico mecanica de salto.
-	if Input.is_action_just_pressed("saltar"):
+	if Input.is_action_just_pressed("saltar") and is_on_floor():
 		velocity.y = velocity.y + salto
-		print("salta")
-	move_and_slide()	
+	# Si esta en el aire y presiona "espacio" activo bandera.
+	if Input.is_action_just_pressed("saltar") and not is_on_floor():
+		saltoBaston = true
+	# Al soltar "espacio" desactivo bandera.
+	if Input.is_action_just_released("saltar"):
+		saltoBaston = false
+	# Realizo el salto con bastón.
+	if is_on_floor() and saltoBaston:
+		velocity.y = velocity.y - 1000
+	
+	move_and_slide()
+	
