218d3f65f7627bbedfa24ee82549bdc926de9638
SrLito <74510505+SrLito@users.noreply.github.com>
2024-12-10 23:04:33 -0300

    Aplique la mecanica de escalar en aro
    Además corregí un problema de dirección con respecto a la escalda de la soga.

--- scripts/jugador.gd
+++ scripts/jugador.gd
@@ -10,6 +10,8 @@ extends CharacterBody2D
 # Variables de estado del personaje.
 var saltoBaston = false
 var colisionCuerda = false
+var colgadoCuerda = false
+var colisionAro = false
 var estaColgado = false
 
 # Funciones de la maquina de estados. // MAS ADELANTE LAS AGREGO
@@ -53,30 +55,43 @@ func _physics_process(delta):
 	if is_on_floor() and saltoBaston:
 		velocity.y = velocity.y - 1000
 	
-	## MÉCANICA DE ESCALAR
-	# Agarrar la cuerda.
-	if colisionCuerda and (Input.is_action_just_pressed("bajar") or Input.is_action_just_pressed("subir")) :
+	## MÉCANICA DE COLGARSE.
+	# Agarrar la cuerda o aro.
+	if (colisionCuerda or colisionAro) and (Input.is_action_just_pressed("bajar") or Input.is_action_just_pressed("subir")) :
 		estaColgado = true
 		velocity = Vector2.ZERO
 	 # Subir o bajar por la cuerda.
-	if estaColgado:
-		var direccion_cuerda = Input.get_axis("bajar", "subir")
-		velocity.y = -direccion_cuerda * 100
+	if estaColgado and colgadoCuerda:
+		var direccion_cuerda = Input.get_axis("subir", "bajar")
+		velocity.y = direccion_cuerda * 100
 	# Soltar la cuerda.
 	if estaColgado:
 		if Input.is_action_just_pressed("saltar"):
+			velocity.y = velocity.y + salto
 			estaColgado = false
 	
-
 	move_and_slide()    
 
-
+# Personaje colisiona con la cuerda.
 func _on_cuerda_body_entered(_body: Node2D) -> void:
 	# Estado del personaje con respecto a la cuerda.
 	colisionCuerda = true
+	colgadoCuerda = true
 	
+# Personaje sale de la colision con la cuerda.
 func _on_cuerda_body_exited(_body: Node2D) -> void:
 	# Estado del personaje con respecto a la cuerda.
 	colisionCuerda = false
 	# Si el personaje sale de la cuerda.
 	estaColgado = false
+	colgadoCuerda = false
+
+# Personaje colisiona con el aro.
+func _on_aro_body_entered(_body: Node2D) -> void:
+	# Estado del personaje con respecto al aro.
+	colisionAro = true
+
+# Personaje sale de la colision con el aro.
+func _on_aro_body_exited(body: Node2D) -> void:
+	# Si el personaje sale del aro.
+	colisionAro = false
